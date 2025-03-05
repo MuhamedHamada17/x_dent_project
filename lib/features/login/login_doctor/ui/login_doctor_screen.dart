53c7c1dcc17f4288bd907d34bdd55a0f42e20b43
MuhamedHamada <mohamedsscc11@gmail.com>
2025-03-05 16:39:09 +0200

    finish splash_Screen and onBoarding_screen

--- lib/features/login/login_doctor/ui/login_doctor_screen.dart
+++ lib/features/login/login_doctor/ui/login_doctor_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:x_dent_project/core/theiming/colors.dart';
 
 class LoginDoctorScreen extends StatelessWidget {
   const LoginDoctorScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: ColorsManager.lighterBLUE,
+    );
   }
 }
