2367b20c68b39877a2a09f54206a18b123898fb6
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-03-08 02:47:39 +0200

    add int x in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
+import 'core/routing/routes.dart';
 import 'package:flutter/cupertino.dart';
+import 'core/di/dependency_injection.dart';
 import 'package:x_dent_project/x_dent_app.dart';
 
-import 'core/di/dependency_injection.dart';
-import 'core/routing/routes.dart';
-
 void main() {
+  // test the push
+  int x = 5;
   setupGetIt();
-  runApp( xDentApp(
-    appRouter: AppRouter(),
-  ) as Widget);
+  runApp(xDentApp(appRouter: AppRouter()) as Widget);
 }
