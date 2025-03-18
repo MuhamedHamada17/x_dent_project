abce2506461331d864028a5ca4afb4536aa38308
OmarEidAwad <152607793+OmarEidAwad@users.noreply.github.com>
2025-03-18 16:59:34 +0200

    edit sign up file

--- lib/features/login/login_doctor/ui/login_doctor_screen.dart
+++ lib/features/login/login_doctor/ui/login_doctor_screen.dart
@@ -9,7 +9,7 @@ import 'package:x_dent_project/core/widgets/app_text_form_field.dart';
 import 'package:x_dent_project/features/login/login_patient/ui/widgets/app_bar_login.dart';
 import 'package:x_dent_project/features/login/login_patient/ui/widgets/log_with.dart';
 
-import 'package:x_dent_project/features/signUp/ui/screens/signup_doctor/sign_up_doctor_screen1.dart';
+import 'package:x_dent_project/features/signUp/doctor/ui/screens/signup_doctor/sign_up_doctor_screen1.dart';
 
 import '../../../../core/di/dependency_injection.dart';
 import '../../../../core/helpers/spacing.dart';
--- lib/features/signUp/doctor/ui/screens/signup_doctor/sign_up_doctor_screen1.dart
+++ lib/features/signUp/doctor/ui/screens/signup_doctor/sign_up_doctor_screen1.dart
@@ -7,12 +7,11 @@ import 'package:x_dent_project/core/theiming/colors.dart';
 import 'package:x_dent_project/core/theiming/styles.dart';
 import 'package:x_dent_project/core/widgets/app_text_button.dart';
 import 'package:x_dent_project/core/widgets/app_text_form_field.dart';
-import 'package:x_dent_project/features/home/doctor/doctor_home_page/ui/screens/doctor_home_page/doctor_home_screen.dart';
 import 'package:x_dent_project/features/login/login_doctor/ui/login_doctor_screen.dart';
-import 'package:x_dent_project/features/signUp/ui/screens/widgets/upload_cv_field.dart';
+import 'package:x_dent_project/features/signUp/doctor/ui/screens/signup_doctor/sign_up_doctor_screen2.dart';
 
-class SignUpDoctorScreen2 extends StatelessWidget {
-  const SignUpDoctorScreen2({super.key});
+class SignUpDoctorScreen1 extends StatelessWidget {
+  const SignUpDoctorScreen1({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -40,11 +39,11 @@ class SignUpDoctorScreen2 extends StatelessWidget {
                   ],
                 ),
               ),
-              verticalSpace(25),
+              verticalSpace(15),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text("  Degree", style: TextStyles.font14BlackSemiBold),
+                  Text("  First name", style: TextStyles.font14BlackSemiBold),
                 ],
               ),
               verticalSpace(8),
@@ -69,7 +68,108 @@ class SignUpDoctorScreen2 extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text("  University", style: TextStyles.font14BlackSemiBold),
+                  Text("  Last name", style: TextStyles.font14BlackSemiBold),
+                ],
+              ),
+              verticalSpace(8),
+              SizedBox(
+                width: 320.w,
+                height: 46.h,
+                child: AppTextFormField(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: ColorsManager.Grey.withOpacity(.8),
+                      width: 1.3,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1.3),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+              verticalSpace(12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("  Email", style: TextStyles.font14BlackSemiBold),
+                ],
+              ),
+              verticalSpace(8),
+              SizedBox(
+                width: 320.w,
+                height: 46.h,
+                child: AppTextFormField(
+                  hintText: "you@email.com",
+                  hintStyle: TextStyles.font14GreyRegular,
+                  prefixIcon: Icon(Icons.email_outlined),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: ColorsManager.Grey.withOpacity(.8),
+                      width: 1.3,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1.3),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+              verticalSpace(12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("  Phone", style: TextStyles.font14BlackSemiBold),
+                ],
+              ),
+              verticalSpace(8),
+              SizedBox(
+                width: 320.w,
+                height: 46.h,
+                child: AppTextFormField(
+                  prefixIcon: Row(
+                    children: [
+                      horizontalSpace(14),
+                      Text(
+                        "EG",
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 112, 112, 112),
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(Icons.chevron_right),
+                      horizontalSpace(24),
+                      Text(
+                        "+1 (000) 000-0000",
+                        style: TextStyle(
+                          color: ColorsManager.Grey.withOpacity(.8),
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: ColorsManager.Grey.withOpacity(.8),
+                      width: 1.3,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 1.3),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+              verticalSpace(15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("  Password", style: TextStyles.font14BlackSemiBold),
                 ],
               ),
               verticalSpace(8),
@@ -95,7 +195,7 @@ class SignUpDoctorScreen2 extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Text(
-                    "  Year Graduated",
+                    "  Confirm Password",
                     style: TextStyles.font14BlackSemiBold,
                   ),
                 ],
@@ -118,101 +218,33 @@ class SignUpDoctorScreen2 extends StatelessWidget {
                   ),
                 ),
               ),
-              verticalSpace(12),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    "  Specialization",
-                    style: TextStyles.font14BlackSemiBold,
-                  ),
-                ],
-              ),
-              verticalSpace(8),
-              SizedBox(
-                width: 320.w,
-                height: 46.h,
-                child: AppTextFormField(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: ColorsManager.Grey.withOpacity(.8),
-                      width: 1.3,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black, width: 1.3),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-              ),
-              verticalSpace(12),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text("  Location", style: TextStyles.font14BlackSemiBold),
-                ],
-              ),
-              verticalSpace(8),
-              SizedBox(
-                width: 320.w,
-                height: 46.h,
-                child: AppTextFormField(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: ColorsManager.Grey.withOpacity(.8),
-                      width: 1.3,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black, width: 1.3),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-              ),
-              verticalSpace(12),
-
-              UploadCVField(),
               verticalSpace(32),
               Center(
                 child: SizedBox(
                   width: 200.w,
                   height: 45.h,
-                  child: GestureDetector(
-                    onTap:
+                  child: AppTextButton(
+                    buttonText: "Next",
+                    borderRadius: 20,
+                    verticalPadding: 4.h,
+                    backgroundColor: ColorsManager.Blue,
+                    textStyle: TextStyles.font20whiteRegular,
+                    onPressed:
                         () => Navigator.push(
                           context,
                           MaterialPageRoute(
                             builder: (context) {
-                              return DoctorHomeScreen();
+                              return SignUpDoctorScreen2();
                             },
                           ),
                         ),
-                    child: AppTextButton(
-                      buttonText: "Continue",
-                      borderRadius: 20,
-                      verticalPadding: 4.h,
-                      backgroundColor: ColorsManager.Blue,
-                      textStyle: TextStyles.font20whiteRegular,
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return DoctorHomeScreen();
-                            },
-                          ),
-                        );
-                      },
-                    ),
                   ),
                 ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  verticalSpace(70),
+                  verticalSpace(90),
                   Text(
                     "Already have an account? ",
                     style: TextStyles.font12BlackRegular,
