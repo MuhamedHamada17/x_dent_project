cb58acdc6335d66e66b93af27f0cd84289475a0e
OmarEidAwad <152607793+OmarEidAwad@users.noreply.github.com>
2025-06-02 14:07:42 +0300

    .

--- lib/features/home/doctor/doctor_home_page/data/repos/doctor_home_repo.dart
+++ lib/features/home/doctor/doctor_home_page/data/repos/doctor_home_repo.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:x_dent_project/core/networking/api_constants.dart';
 import 'package:x_dent_project/core/networking/api_error_model.dart';
 import 'package:x_dent_project/core/networking/api_service.dart';
 import 'package:x_dent_project/features/home/doctor/doctor_home_page/data/models/doctor_home_response_model.dart';
@@ -11,7 +10,6 @@ class DoctorHomeRepository {
 
   Future<DoctorHomeResponseModel> getDoctorHomeData(String token) async {
     try {
-      // إضافة كلمة Bearer للتوكن
       final bearerToken = 'Bearer $token';
       final response = await _apiService.getDoctorHome(bearerToken);
       return response;
